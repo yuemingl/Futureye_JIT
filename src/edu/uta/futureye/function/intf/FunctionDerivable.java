17bbef8f163df1679c0211dfa47f6f4fe9152e11
nkliuyueming@gmail.com <nkliuyueming@gmail.com@ab3864fb-8184-af3f-d63f-195d806ec97b>
2011-03-11 04:20:21 +0000

    Delete unused files

--- src/edu/uta/futureye/function/intf/FunctionDerivable.java
+++
@@ -1,7 +0,0 @@
-package edu.uta.futureye.function.intf;
-
-import edu.uta.futureye.function.DerivativeIndicator;
-
-public interface FunctionDerivable extends Function {
-	public FunctionDerivable derivative(DerivativeIndicator di);
-}
