04a1b6e86bfc2c5fc1f4074280d8f93f1d9947ee
yueming <nkliuyueming@gmail.com>
2015-02-02 22:00:42 -0800

    make it works

--- src/edu/uta/futureye/test/LambdaTest.java
+++ src/edu/uta/futureye/test/LambdaTest.java
@@ -9,13 +9,13 @@ public class LambdaTest {
 		public double check(double a, double b);
 	}
 	
-	public static void main(String[] args) {
-		MathFun f = C1.A(100);
-		Checker ck = (double a, double b) -> { 
-				return f.A(a+b).apply(new Variable(50.0)); 
-			};
-		double r = ck.check(10, 2);
-		System.out.println(r);
-	}
+//	public static void main(String[] args) {
+//		MathFun f = C1.A(100);
+//		Checker ck = (double a, double b) -> { 
+//				return f.A(a+b).apply(new Variable(50.0)); 
+//			};
+//		double r = ck.check(10, 2);
+//		System.out.println(r);
+//	}
 
 }
