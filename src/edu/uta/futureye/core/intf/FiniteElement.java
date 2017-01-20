057e12e69d1423bd258a73cff5a7e6406ca56d2d
yuemingl <nkliuyueming@gmail.com>
2017-01-19 23:01:25 -0800

    add assignTo(Element e) to FiniteElement

--- src/edu/uta/futureye/core/intf/FiniteElement.java
+++ src/edu/uta/futureye/core/intf/FiniteElement.java
@@ -2,12 +2,19 @@ package edu.uta.futureye.core.intf;
 
 import java.util.Map;
 
+import edu.uta.futureye.core.Element;
 import edu.uta.futureye.function.intf.MathFunc;
 
 public interface FiniteElement {
-	MathFunc[] getShapeFunctions();
 	int getNumberOfDOFs();
+	
+	MathFunc[] getShapeFunctions();
+	
 	Map<String, MathFunc> getCoordTransMap();
+	
 	String[] getArgsOrder();
+	
 	MathFunc getJacobian();
+	
+	void assignTo(Element e);
 }
